11258288dd3c7ed8e50e1fc1f6f9a913a2644117
Paul Thomas <paulthomas2107@gmail.com>
2022-11-14 12:06:57 +0000

    More....

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    mode: 'development',    
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name][contenthash].js'
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ],
 }
